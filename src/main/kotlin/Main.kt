443dfd64a5d065c49134480a2b3bdc84688382a5
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-01-10 14:26:48 +0100

    asyncronous code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,28 @@
 package org.example
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
+//Coroutines
+//Co -> Cooperative
+//Routines -> a set of tasks, like functions
 fun main() {
     val time = measureTimeMillis {
         runBlocking {
             println("Fetching news...")
-            printNews()
-            printArticle()
+            launch {
+                printNews()
+            }
+            launch {
+                printArticle()
+            }
+
+            println("Connection closed")
         }
     }
     println("Execution time: ${time / 1000} seconds")
-
 }
 
 suspend fun printNews() {
