1f66502755d85a3d982a5b8e92b4b242b484be16
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2025-01-10 14:17:57 +0100

    syncronous code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,27 @@
 package org.example
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
 
-suspend fun main() {
-    println("Fetching news...")
+fun main() {
+    val time = measureTimeMillis {
+        runBlocking {
+            println("Fetching news...")
+            printNews()
+            printArticle()
+        }
+    }
+    println("Execution time: ${time / 1000} seconds")
+
+}
+
+suspend fun printNews() {
     delay(1000)
     println("NEWS")
+}
+
+suspend fun printArticle() {
+    delay(1000)
+    println("Article 1")
 }
